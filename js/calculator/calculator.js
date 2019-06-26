9ca056f136a0ccee3870b6ba5ae5d8fa57e5ad2b
JayKay <jaykay91.me@gmail.com>
2019-06-27 03:38:53 +0900

    입력된 숫자와 연산자 저장하는 기능 추가

--- js/calculator/calculator.js
+++ js/calculator/calculator.js
@@ -3,62 +3,14 @@ const numberBtns = document.querySelectorAll('.js-num-btn')
 const backBtn = document.querySelector('.js-back-btn')
 const cBtn = document.querySelector('.js-c-btn')
 
-const createRender = ({ selector, stateName }) => {
+const createRender = ({ selector, stateName, renderData }) => {
   const element = document.querySelector(selector)
-  const mapper = {
-    [stateName]: element
-  }
+  const mapper = { [stateName]: element }
   
   return state => {
-    const val = state[stateName]
+    const val = renderData(state[stateName])
     mapper[stateName].textContent = val
   }
-}
-const render = createRender({ 
-  selector: '.js-screen', 
-  stateName: 'screen', 
-})
-
-let state = {
-  screen: '0',
-  buffers: [],
-  last: null,
-  number: 0,
-  operation: null,
-}
-
-const setState = newState => state = Object.assign({}, state, newState)
-const getState = () => state
-
-render(state);
-
-const operations = {
-  ['+'](a, b) { return a + b },
-  ['-'](a, b) { return a - b },
-  ['*'](a, b) { return a*b },
-  ['/'](a, b) { return a/b },
-}
-
-const updateArray = (oldarr, ...args) => [...oldarr, ...args]
-const changeLastBuffer = oldarr => {
-  if (oldarr.length === 0) {
-
-  }
-
-
-  return newarr
-}
-
-const changeStrnumToNumber = strnum => parseInt(strnum)
-
-const concatNumber = (oldnum, newnum) => {
-  if (oldnum === '0') return newnum
-  return `${oldnum}${newnum}`
-}
-
-const normalizeNumber = strnum => {
-  const arr = strnum.split(',')
-  return arr.join('')
 }
 
 const makeNumberUsingComma = num => {
@@ -74,64 +26,120 @@ const makeNumberUsingComma = num => {
   return strbuf
 }
 
-const eraseLastNumber = strnum => 
-  strnum.length === 1 ? strnum : strnum.slice(0, strnum.length - 1)
+const render = createRender({ 
+  selector: '.js-screen', 
+  stateName: 'number', 
+  renderData(val) {
+    return makeNumberUsingComma(val)
+  }
+})
+
+let state = {
+  buffers: [],
+  number: 0,
+  calculated: 0,
+  operator: null,
+  wait: 'NUMBER',
+}
+
+const setState = newState => state = Object.assign({}, state, newState)
+const getState = () => state
+
+render(state);
+
+const createMatch = (mapper) => {
+  return (branch, state) => mapper[branch](state)
+}
+
+const createMap = (mapper) => {
+  return prop => mapper[prop]
+}
+
+const mapOperator = createMap({
+  ['+'](a, b) { return a + b },
+  ['-'](a, b) { return a - b },
+  ['*'](a, b) { return a*b },
+  ['/'](a, b) { return a/b },
+})
+
+const updateArray = (oldarr, ...args) => [...oldarr, ...args]
+
+const concatNumber = (oldnum, newnum) => {
+  if (`${oldnum}` === '0') return newnum
+  return parseInt(`${oldnum}${newnum}`)
+}
+
+const eraseLastNumber = number => {
+  const strnum = `${number}`
+  const slstr = strnum.length === 1 ? 
+    strnum : strnum.slice(0, strnum.length - 1)
+  return parseInt(slstr)
+}
+
+
+
 
 const onClickButton = {
   number(newnum) {
-    const { screen: oldnum } = getState()
+    const { number, wait, operator, buffers } = getState()
 
-    
-    const normnum = normalizeNumber(oldnum)
-    const connum = concatNumber(normnum, newnum)
-    const number = parseInt(connum)
-    const commanum = makeNumberUsingComma(connum)
-    setState({ screen: commanum, last: 'number', number })
+    const matchWaitState = createMatch({
+      NUMBER({ number, newnum }) {
+        const connum = concatNumber(number, newnum)
+        return { number: connum }
+      },
+      OPERATOR({ newnum, operator, buffers }) {
+        const newBuffers = updateArray(buffers, operator)
+        return { wait: 'NUMBER', number: newnum, buffers: newBuffers }
+      },
+    })
+    const nextState = matchWaitState(wait, { number, newnum, operator, buffers })
+
+    setState(nextState)
   },
-  operation(operation) {
-    const { buffers, last, number } = getState()
+  operator(operator) {
+    const { buffers, number, wait } = getState()
 
-    if (last === 'number') {
-      const newbuffers = updateArray(buffers, number)
-      setState({ 
-        last: 'operation', 
-        buffers: newbuffers,
-        operation,
-      })
-      return
-    } 
+    const matchWaitState = createMatch({
+      NUMBER({ buffers, number, operator }) {
+        // 결과값이 있는 경우 출력 아니면 과거값 그대로 표시 구현 필요
+        const newBuffers = updateArray(buffers, number)
+        const wait = 'OPERATOR'
+        return { buffers: newBuffers, wait, operator }
+      },
+      OPERATOR({ operator }) {
+        return { operator }
+      },
+    })
 
-    if (last === 'operation') {
-      setState({ 
-        last: 'operation', 
-        operation,
-      })
-      return
-    }
+    const nextState = matchWaitState(wait, { buffers, number, operator })
+    setState(nextState)
   },
   c() {
-    setState({ screen: '0', number: 0 })
+    setState({ number: 0, wait: 'NUMBER', buffers: [] })
   },
   back() {
-    const { screen: curnum } = getState()
-    const nornum = normalizeNumber(curnum)
-    const eranum = eraseLastNumber(nornum)
-    const number = parseInt(eranum)
-    const comnum = makeNumberUsingComma(eranum)
-    setState({ screen: comnum, number })
+    const { number, wait } = getState()
+
+    if (wait === 'OPERATOR') return
+
+    const eranum = eraseLastNumber(number)
+    setState({ number: eranum })
   },
 }
 
 
+
+
 operationBtns.forEach(el => el.addEventListener('click', e => {
   const op = e.target.textContent
-  onClickButton.operation(op)
+  onClickButton.operator(op)
   console.log(state)
 }))
 
 numberBtns.forEach(el => el.addEventListener('click', e => {
   const number = e.target.textContent
-  onClickButton.number(number)
+  onClickButton.number(parseInt(number))
   render(state)
   console.log(state)
 
@@ -148,7 +156,6 @@ cBtn.addEventListener('click', e => {
 /*
   할 일 목록
     1. 연산 기능 추가
-    2. 렌더링 기능 추가
   
   공부 목록
     0. 축약 패턴
