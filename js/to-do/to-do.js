ae4acfb5fd113d523e5cbb14c4c52729a347d14a
JayKay <jaykay91.me@gmail.com>
2019-09-06 22:27:25 +0900

    to-do 수정

--- js/to-do/to-do.js
+++ js/to-do/to-do.js
@@ -25,8 +25,11 @@ const createStore = (initState, render) => {
   let currState = initState
   
   return {
-    setState(updatedState) {
-      if (updatedState) {
+    setState(action) {
+      if (action) {
+        const [updatedState, actionType] = action
+
+         
         prevState = currState
         currState = Object.assign({}, currState, updatedState)
         render(updatedState, prevState)
@@ -40,19 +43,25 @@ const createStore = (initState, render) => {
 }
 
 const actionMap = [
+  // {
+  //   action: 'CHANGE_TODO',
+  //   selectors: {
+  //     todoContainer: '.js-TodoContainer',
+  //   },
+  //   render: ({ todoList }, { todoContainer }) => {
+  //     const lastTodo = todoList.slice(-1)[0]
+  //     const todo = document.createElement('div')
+  //     let content = 
+  //                 `<div><input type="checkbox"><span>${lastTodo}</span></div>`
+  //     content += `<div><button>수정</button><button>삭제</button></div>`
+  //     todo.innerHTML = content
+  //     todoContainer.append(todo)        
+  //   }
+  // },
   {
-    action: 'CHANGE_TODO',
+    action: 'CHANGE_VALUE',
     selectors: {
-      todoContainer: '.js-TodoContainer',
-    },
-    render: ({ todoList }, { todoContainer }) => {
-      const lastTodo = todoList.slice(-1)[0]
-      const todo = document.createElement('div')
-      let content = 
-                  `<div><input type="checkbox"><span>${lastTodo}</span></div>`
-      content += `<div><button>수정</button><button>삭제</button></div>`
-      todo.innerHTML = content
-      todoContainer.append(todo)        
+      
     }
   },
 ]
@@ -66,10 +75,10 @@ const store = createStore(state, render)
 
 const hookMap = {
   changeValue: (state, target) => {
-    return { inputValue: target.value }
+    return [{ inputValue: target.value }]
   },
   addTodo: ({ todoList, inputValue }) => {
-    return { todoList: [...todoList, inputValue] }    
+    return [{ todoList: [...todoList, inputValue] }, 'CHANGE_TODO']    
   },
   deleteTodo: (state) => {
 
@@ -80,12 +89,12 @@ const hookMap = {
 }
 
 document.querySelectorAll('[class^="js-"]').forEach(el => {
-  const action = el.dataset.action
+  const hook = el.dataset.hook
   const event = el.dataset.event
   el.addEventListener(event, e => {
     const prev = store.getState()
-    const updated = actionMap[action](prev, e.target)
-    store.setState(updated)
+    const action = hookMap[hook](prev, e.target)
+    store.setState(action)
   })
 })
 
