c07863cca21bbd5e600b11163b4da612b817d1f9
JayKay <jaykay91.me@gmail.com>
2019-09-09 22:39:48 +0900

    to-do: 추가 기능 생성

--- js/to-do/to-do.js
+++ js/to-do/to-do.js
@@ -1,121 +1,119 @@
-console.log('Hello, To-Do!')
+class Store {
+  currState = {
+    inputValue: '',
+    todoList: [],
+  }
 
-const createRender = stateMap => {
-  const domMap = stateMap.map(([[state, selector], render]) => {
-    const dom = document.querySelector(selector)
-    return [[state, dom], render]
-  })
+  prevState = null
 
-  return (updatedObj, prevStateObj) => {
-    const entries = Object.entries(updatedObj)
-    for (const [updatedState, updatedValue] of entries) {
-      for (const [[state, dom], render] of domMap) {
-        if (updatedState === state) {
-          const prevValue = prevStateObj[updatedState]
-          render(updatedValue, dom, prevValue)
-          break
+  setState(updatedState) {
+    this.prevState = this.currState
+    this.currState = Object.assign({}, this.currState, updatedState)
+  }
+
+  getState() {
+    return this.currState
+  }
+}
+
+class ActionMap {
+  constructor (store) {
+    this.store = store
+  }
+
+  domMap = {}
+  functionMap = {}
+
+  async useAction(action, ...args) {
+    const functions = this.functionMap[action]
+    for (const func of functions) {
+      const state = this.store.getState()
+      const result = await func.call(this.domMap, state)
+      if (result) {
+        if (typeof result === 'object') {
+          this.store.setState(result)
         }
-      }
-    }
-  }
-}
-
-const createStore = (initState, render) => {
-  let prevState
-  let currState = initState
-  
-  return {
-    setState(action) {
-      if (action) {
-        const [updatedState, actionType] = action
-
-         
-        prevState = currState
-        currState = Object.assign({}, currState, updatedState)
-        render(updatedState, prevState)
-        // console.log(prevState, currState)
-      }
-    },
-    getState() {
-      return currState
-    },
-  }
-}
-
-const actionMap = [
-  // {
-  //   action: 'CHANGE_TODO',
-  //   selectors: {
-  //     todoContainer: '.js-TodoContainer',
-  //   },
-  //   render: ({ todoList }, { todoContainer }) => {
-  //     const lastTodo = todoList.slice(-1)[0]
-  //     const todo = document.createElement('div')
-  //     let content = 
-  //                 `<div><input type="checkbox"><span>${lastTodo}</span></div>`
-  //     content += `<div><button>수정</button><button>삭제</button></div>`
-  //     todo.innerHTML = content
-  //     todoContainer.append(todo)        
-  //   }
-  // },
-  {
-    action: 'CHANGE_VALUE',
-    selectors: {
-
-    }
-  },
-]
-
-const state = {
-  inputValue: '',
-  todoList: [],
-}
-const render = createRender($map)
-const store = createStore(state, render)
-
-const hookMap = {
-  changeValue: (state, target) => {
-    return [{ inputValue: target.value }]
-  },
-  addTodo: ({ todoList, inputValue }) => {
-    return [{ todoList: [...todoList, inputValue] }, 'CHANGE_TODO']    
-  },
-  deleteTodo: (state) => {
-
-  },
-  modifyTodo: (state) => {
-
-  },
-}
-
-document.querySelectorAll('[class^="js-"]').forEach(el => {
-  // const hook = el.dataset.hook
-  const action = el.dataset.event
-
-  (() => {
-    const string = 'addTodo'
-    let upperIdx
-    for (const idx in string) {
-      const char = string[idx]
-      
-      if (char === char.toUpperCase()) {
-        upperIdx = idx
+      } else {
         break
-          }
       }
-    const lower = string.slice(0, upperIdx)
-    const upper = string.slice(upperIdx)
-    return [lower, upper]
-  })()
+    }
+  }
 
+  setAction(action, ...functions) {
+    this.functionMap[action] = functions
+  }
+}
+
+const store = new Store()
+const actionMap = new ActionMap(store)
+
+document.querySelectorAll('[class*="js-"]').forEach(el => {
+  const hook = el.dataset.hook
   
-  
-  el.addEventListener(event, e => {
-    const prev = store.getState()
-    const action = hookMap[hook](prev, e.target)
-    store.setState(action)
-  })
+  if (hook) {
+    const [event, action] = hook.split(';')
+    el.addEventListener(event, e => {
+      actionMap.domMap.target = e.target
+      actionMap.useAction(action)
+    })
+  }
+
+  const className = el.className.split(' ').find(name => name.includes('js-'))
+  const domName = className.replace('js-', '')
+  actionMap.domMap[domName] = el
 })
 
 
 
+
+
+
+// actionMap.setAction('ADD_TODO', function ({ todoList, inputValue }) {
+//   return { todoList: [...todoList, inputValue], inputValue: '' }
+// }, function ({ todoList }) {
+//   const { InputBox_input, TodoContainer } = this
+//   InputBox_input.value = ''
+//   const lastTodo = todoList.slice(-1)[0]
+//   const todo = document.createElement('div')
+//   const content = ` 
+//     <div><input type="checkbox"><span>${lastTodo}</span></div>
+//     <div><button>수정</button><button>삭제</button></div>
+//   `
+//   todo.innerHTML = content
+//   TodoContainer.append(todo)        
+// })
+
+
+actionMap.setAction('ADD_TODO', ({ todoList, inputValue }) => {
+  if (!inputValue) return null
+  return { todoList: [...todoList, inputValue], inputValue: '' }
+}, function (state) {
+  refreshTodo.call(this, state)
+  updateValue.call(this, state)
+})
+
+actionMap.setAction('CONTROL_TODO', function (state) {
+  console.log(this.target)
+})
+
+
+actionMap.setAction('CHANGE_VALUE', function () {
+  return { inputValue: this.target.value }
+})
+
+function updateValue({ inputValue }) {
+  const { InputBox_input } = this
+  InputBox_input.value = inputValue
+}
+
+function refreshTodo({ todoList }) {
+  const { TodoContainer } = this
+  const TodoList = todoList.map(content => `
+    <div>
+      <div><input type="checkbox"><span>${content}</span></div>
+      <div><button>수정</button><button>삭제</button></div>
+    </div>
+  `).join('')
+
+  TodoContainer.innerHTML = TodoList
+}
